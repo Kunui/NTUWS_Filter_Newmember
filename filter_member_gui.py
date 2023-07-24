6e9f2a5690af77d094afa04b050f7ad142051c8a
Kunui <jimmychwchang@gmail.com>
2023-07-24 23:05:01 +0800

    GUI modify

--- filter_member_gui.py
+++ filter_member_gui.py
@@ -8,3 +8,24 @@ class filmem_gui():
     def __init__(self) -> None:
         self.win = tk.Tk()
         self.win.geometry("550x500")
+        self.win.title("選擇開啟檔案")
+
+        #Lables
+        self.welcome_questionaire_list_label = tk.Label(text = "歡迎問卷名單")
+        self.total_member_list_label = tk.Label(text = "一般會員名單")
+        self.quarantine_list_label = tk.Label(text = "隔離區名單")
+        
+        #Entries
+        self.welcome_questionaire_list_entry = tk.Entry()
+        self.total_member_list_label_enrty = tk.Entry()
+        self.quarantine_list_label_entry = tk.Entry()
+        self.win.mainloop()
+
+    '''def select_file(self, filetype: str):
+        if filetype == "welcome_questionaire_list":
+
+        if filetype == "total_member_list":
+
+        if filetype == "quarantine_list":'''
+
+filmem_gui()
